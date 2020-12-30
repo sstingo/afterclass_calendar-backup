71dda15b1abb0ef00ccedf85e4d0c77f9fe7e80e
sstingo <48621517+sstingo@users.noreply.github.com>
2020-12-30 16:32:47 +0800

    login

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -15,12 +15,29 @@ class _Login extends State<Login> {
         title: Text('登入'),
       ),
       body: Center(
-        child: Card(
-          child: Column(
-            children: [
-              Text("NID登入"),
-              Text("訪客登入"),
-            ],
+        child: Container(
+          width: 200,
+          height: 150,
+          // decoration: BoxDecoration(color: Colors.red), /////////////////
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  //, backgroundColor: Color(0xAARRGGBB)
+                  Image.asset(
+                    "images/ic_launcher.png",
+                    width: 50,
+                  ),
+                  // ListTile(
+                  //   title: Text("NID登入"),
+                  //   onTap: () {},
+                  // ),
+                  Text("NID登入", style: TextStyle(fontSize: 18)),
+                  Text("訪客登入", style: TextStyle(fontSize: 18)),
+                ],
+              ),
+            ),
           ),
         ),
       ),
